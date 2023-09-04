1bb4bea8af04bc7a353247f96eb8ac772b8a056e
Eduardo Tavares <eduardot.dev@icloud.com>
2023-09-04 14:34:07 -0300

    creating file listing

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -5,7 +5,7 @@ import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
-export function Control(props: ControlProps) {
+export function Control({ multiple, ...props }: ControlProps) {
   const { id, onFilesSelected } = useFileInput()
 
   function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -24,6 +24,7 @@ export function Control(props: ControlProps) {
       className="sr-only"
       id={id}
       onChange={handleFilesSelected}
+      multiple={multiple}
       {...props}
     />
   )
