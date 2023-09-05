1d43d5260de1050d5b6ce4885875cbdfb0df06bd
Eduardo Tavares <eduardot.dev@icloud.com>
2023-09-05 19:02:15 -0300

    listing multiple files

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -5,7 +5,7 @@ import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
-export function Control({ multiple, ...props }: ControlProps) {
+export function Control({ multiple = false, ...props }: ControlProps) {
   const { id, onFilesSelected } = useFileInput()
 
   function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -15,7 +15,7 @@ export function Control({ multiple, ...props }: ControlProps) {
 
     const files = Array.from(event.target.files)
 
-    onFilesSelected(files)
+    onFilesSelected(files, multiple)
   }
 
   return (
